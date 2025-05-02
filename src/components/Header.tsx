774b45b88e29464f462933cf5e5e9a66246c76f6
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-05-02 20:34:14 +0600

    added today's date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import { format } from "date-fns";
 import headerLogo from "../assets/logo.svg";
 
 const Header = () => {
+  const todayDate = format(new Date(), "EEEE, MMMM d, yyyy");
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4 text-center my-4">
+    <div className="flex flex-col items-center justify-center gap-2 text-center my-4">
       <img src={headerLogo} alt="header logo" />
       <p className="text-gray-500">Journalism Without Fear or Favour</p>
+      <p>{todayDate}</p>
     </div>
   );
 };
