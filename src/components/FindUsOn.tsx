43f65e6305d23371e7f257993bc83b2e93ed740a
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-05-10 23:13:52 +0600

    added link in find us on

--- src/components/FindUsOn.tsx
+++ src/components/FindUsOn.tsx
@@ -9,24 +9,39 @@ const FindUsOn = () => {
             text-gray-500 *:hover:text-black 
             *:hover:bg-gray-300 *:p-3 *:cursor-pointer *:flex *:items-center *:gap-2`}
       >
-        <div className=" p-3 border-b border-gray-300">
+        <a
+          href={"https://www.facebook.com/"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className=" p-3 border-b border-gray-300"
+        >
           <span className="rounded-full p-2 bg-gray-100">
             <FaFacebookF className="text-blue-500" />
           </span>{" "}
           Facebook
-        </div>
-        <div className="border-b border-gray-300">
+        </a>
+        <a
+          href={"https://x.com/"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-b border-gray-300"
+        >
           <span className="rounded-full p-2 bg-gray-100">
             <FaTwitter className="text-blue-300" />
           </span>
           Twitter
-        </div>
-        <div className="border-gray-300">
+        </a>
+        <a
+          href={"https://www.instagram.com/"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-gray-300"
+        >
           <span className="rounded-full p-2 bg-gray-100">
-            <FaInstagram className="text-pink-500"/>
+            <FaInstagram className="text-pink-500" />
           </span>{" "}
           Instagram
-        </div>
+        </a>
       </div>
     </div>
   );
