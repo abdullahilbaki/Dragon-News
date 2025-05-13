7413edfc05af610c7deba39c4a8f532171328258
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-05-14 01:48:15 +0600

    edited theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { format } from "date-fns";
 import headerLogo from "../assets/logo.svg";
+import { Link } from "react-router";
 
 const Header = () => {
   const todayDate = format(new Date(), "EEEE, MMMM d, yyyy");
 
   return (
     <div className="flex flex-col items-center justify-center gap-2 text-center my-4">
-      <img src={headerLogo} alt="header logo" />
+      <Link to={"/"}>
+        <img src={headerLogo} alt="header logo" />
+      </Link>
       <p className="text-gray-500">Journalism Without Fear or Favour</p>
       <p>{todayDate}</p>
     </div>
